df152a3b2a545ae7583adc2ccea4437a4c1c0f18
Deewan <dewanrathore483@gmail.com>
2024-01-17 21:14:10 +0500

    tab Layout

---
+++ app/src/main/java/com/rathoreraja/tablayout/ChackFragment.java
@@ -0,0 +1,26 @@
+package com.rathoreraja.tablayout;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ChackFragment extends Fragment {
+
+
+    public ChackFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chack, container, false);
+    }
+}
